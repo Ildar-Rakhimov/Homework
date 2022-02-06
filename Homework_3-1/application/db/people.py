66668ab9cd7b2d1671f896684bdbc19d1d36888d
Ildar Rakhimov <ildar.a.rakhimov@gmail.com>
2022-02-06 23:02:39 +1100

    Homework_3-1 added

---
+++ Homework_3-1/application/db/people.py
@@ -0,0 +1,6 @@
+def get_employees():
+    print('Функция для кадрового состава')
+
+
+if __name__ == '__main__':
+    get_employees()
---
+++ Homework_3-1/application/salary.py
@@ -0,0 +1,6 @@
+def calculate_salary():
+    print('Функция для расчёта заработной платы')
+
+
+if __name__ == '__main__':
+    calculate_salary()
