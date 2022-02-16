11e36277201a427b31bbe3c1a3370665c7e40ba9
Ildar Rakhimov <ildar.a.rakhimov@gmail.com>
2022-02-16 23:05:03 +1100

    Почистил код от мусора

--- Homework_3-2/main.py
+++ Homework_3-2/main.py
@@ -1,18 +1,12 @@
-from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
 import re
 
 pattern = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
 substitution = r'+7 (\2) \3-\4-\5 \6 \7'
 
-
 with open("phonebook_raw.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
 
 if __name__ == '__main__':
 
@@ -36,10 +30,7 @@ if __name__ == '__main__':
     fixed_info_list.sort()
     people_list = fixed_info_list
 
-    pprint(people_list)
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
 with open("phonebook.csv", "w", encoding="utf-8") as f:
     datawriter = csv.writer(f, delimiter=',')
     datawriter.writerows(people_list)
+    
